c3ae3f04d773e007ecf6267f81ab2032f44c14cb
Hajer Zam <hajer.zmazi2004@gmail.com>
2025-08-07 14:34:24 +0200

    fixed header file again :)

--- Minishell-Wizard/minishell.h
+++ Minishell-Wizard/minishell.h
@@ -6,7 +6,7 @@
 /*   By: halzamma <halzamma@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 13:57:49 by halzamma          #+#    #+#             */
-/*   Updated: 2025/08/07 14:01:42 by halzamma         ###   ########.fr       */
+/*   Updated: 2025/08/07 14:33:55 by halzamma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,8 +68,21 @@ typedef struct s_cmd
 	int				input_fd;
 	int				output_fd;
 	struct s_cmd	*next;
+	struct s_cmd	*token;
+	int				heredoc_fd;
+	int				is_heredoc; // flag for heredoc
 	int				is_builtin;
+	char			**envp;
+	int				exit_status;
 }		t_cmd;
+
+typedef struct s_parser
+{
+    t_token			*tokens;
+    t_token			*current;
+    t_cmd			*cmd_list;
+    int				error;
+}		t_parser;
 
 typedef struct s_env
 {
@@ -109,6 +122,59 @@ void		handle_double_quote(t_parse_vars *v);
 
 /* signal handling */
 void		init_signals(void);
+
+/* parser_main.c */
+t_cmd		*parse_command_line(t_token *tokens);
+void		init_parser(t_parser *parser, t_token *tokens);
+int			validate_syntax(t_token *tokens);
+
+/* parser_utils.c */
+t_token		*peek_token(t_parser *parser);
+t_token		*consume_token(t_parser *parser);
+int			expect_token(t_parser *parser, t_token_type type);
+int			is_redirection(t_token_type type);
+int			handle_word_token(t_cmd *cmd, t_parser *parser, int *arg_index, int arg_count);
+int			count_words_until_pipe(t_parser *parser);
+void		print_cmd_tree(t_cmd *cmd, int depth);
+void		print_execution_tree(t_cmd *cmd_list);
+void		print_pipeline_summary(t_cmd *cmd_list);
+
+/* parser_command.c */
+t_cmd		*parse_command(t_parser *parser);
+int			parse_cmd_elements(t_cmd *cmd, t_parser *parser);
+int			validate_parsed_command(t_cmd *cmd, t_parser *parser);
+int			is_builtin_command(char **argv);
+void		finalize_command(t_cmd *cmd);
+
+/* parser_pipeline.c */
+t_cmd		*parse_pipeline(t_parser *parser);
+int			count_pipeline_length(t_cmd *cmd_list);
+int			validate_pipeline(t_cmd *cmd_list);
+
+/* parser_redirect.c */
+int			parse_redirections(t_parser *parser, t_cmd *cmd);
+int			handle_input_redirection(t_cmd *cmd, const char *filename);
+int			handle_output_redirection(t_cmd *cmd, const char *filename, int append);
+int			handle_heredoc_redirection(t_cmd *cmd, const char *delimiter);
+int			has_input_redirection(t_cmd *cmd);
+int			has_output_redirection(t_cmd *cmd);
+void		get_redirection_info(t_cmd *cmd, char *buffer, size_t size);
+
+/* parser_memory.c */
+t_cmd		*create_cmd(void);
+void		free_cmd(t_cmd *cmd);
+void		free_cmd_list(t_cmd *cmd_list);
+void		free_argv(char **argv);
+char		*duplicate_string(const char *str);
+char		**allocate_argv(int argc);
+void		cleanup_parser_error(t_parser *parser, t_cmd *partial_cmd);
+void		safe_cmd_append(t_cmd **head, t_cmd *new_cmd);
+
+/* test_main.c */
+void		test_single_case(const char *input, int should_succeed);
+void		run_all_tests(void);
+void		interactive_mode(void);
+void		print_usage(const char *program_name);
 
 /* builtins */
 
