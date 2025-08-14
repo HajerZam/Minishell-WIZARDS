9a33bc760e0dfaa1c25fa4d7e3f223821321a718
Hajer Zam <hajer.zmazi2004@gmail.com>
2025-08-14 14:37:46 +0200

    finished env

--- Minishell-Wizard/builtins/env.c
+++ Minishell-Wizard/builtins/env.c
@@ -12,36 +12,25 @@
 
 #include "../minishell.h"
 
-/* Print all environment variables in key=value format */
-static void	print_env_list(t_env *env)
+static void print_env_list(t_env *env)
 {
-	while (env)
-	{
-		if (env->value)
-			printf("%s=%s\n", env->key, env->value);
-		else
-			printf("%s=\n", env->key);
-		env = env->next;
-	}
+    while (env)
+    {
+        if (env->value)
+            printf("%s=%s\n", env->key, env->value);
+        else
+            printf("%s=\n", env->key);
+        env = env->next;
+    }
 }
 
-/* Main env builtin function 
-	Basic env implementation - just print all environment variables
-	bash env has more options (-i, -u, etc.) but this covers basic functionality
-	env with arguments not supported in basic implementation
-*/
-int	builtin_env(char **args, t_env **env)
+// FIXED: Consistent signature - takes only t_env*
+int builtin_env(t_env *env)
 {
-	if (args && args[0])
-	{
-		fprintf(stderr, "env: '%s': No such file or directory\n", args[0]);
-		return (1);
-	}
-	if (!env || !*env)
-	{
-		fprintf(stderr, "env: No environment variables set\n");
-		return (1);
-	}
-	print_env_list(*env);
-	return (0);
+    if (!env)
+    {
+        return (0);
+    }
+    print_env_list(env);
+    return (0);
 }
--- Minishell-Wizard/builtins/pwd.c
+++ Minishell-Wizard/builtins/pwd.c
@@ -12,15 +12,3 @@
 
 #include "../minishell.h"
 
-int builtin_pwd(void)
-{
-	char	cwd[PATH_MAX];
-
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		perror("pwd");
-		return (1);
-	}
-	printf("%s\n", cwd);
-	return (0);
-}
