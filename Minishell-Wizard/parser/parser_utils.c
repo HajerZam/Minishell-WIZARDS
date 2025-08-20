1559b85f251cbd131c696b3118195251611232c8
Federico Pennarola <fepennar@e3r3p5.42roma.it>
2025-08-20 15:46:04 +0200

    fixEd thE E problEm

--- Minishell-Wizard/parser/parser_utils.c
+++ Minishell-Wizard/parser/parser_utils.c
@@ -40,10 +40,10 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 int	is_redirection(t_token_type type)
